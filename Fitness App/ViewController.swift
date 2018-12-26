39bf037cc05091d5af1dd5757de9e20cfd147b15
Sergey Kletsov <sergey.kletsov@outlook.com>
2018-12-26 18:01:51 +0200

    Initial Commit

---
+++ Fitness App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness App
+//
+//  Created by scales on 12/26/18.
+//  Copyright © 2018 kpi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
