805c7ea1ed0060f042e2c26f4d3148c07ed0f25a
Sergey Kletsov <sergey.kletsov@outlook.com>
2019-01-08 20:29:24 +0200

    add tabbar with custom ui and skeleton tabs

--- Fitness App/Presentation/MainTabBar/TabBarViewController.swift
+++ Fitness App/Presentation/MainTabBar/TabBarViewController.swift
@@ -12,10 +12,27 @@ class TabBarViewController: UITabBarController {
 
 	private var isFirstLoad = true
 	
-    override func viewDidLoad() {
+	private lazy var defaultTabBarHeight = { tabBar.frame.size.height }()
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
+		tabBar.layer.borderWidth = 0
+		tabBar.layer.borderColor = UIColor.clear.cgColor
+		
+		setupShadow()
     }
+	
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		
+		let newTabBarHeight = defaultTabBarHeight - 7
+		
+		var newFrame = tabBar.frame
+		newFrame.size.height = newTabBarHeight
+		newFrame.origin.y = view.frame.size.height - newTabBarHeight
+		
+		tabBar.frame = newFrame
+	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
@@ -23,6 +40,24 @@ class TabBarViewController: UITabBarController {
 			isFirstLoad = false
 			performSegue(withIdentifier: .presentLoginSegueIdentifier, sender: nil)
 		}
+	}
+	
+	private func setupShadow() {
+		let tabGradientView = UIView(frame: tabBar.bounds)
+		tabGradientView.backgroundColor = UIColor.white
+		tabGradientView.translatesAutoresizingMaskIntoConstraints = false
+		
+		tabBar.addSubview(tabGradientView)
+		tabBar.sendSubviewToBack(tabGradientView)
+		tabGradientView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		
+		tabGradientView.layer.shadowOffset = CGSize(width: 0, height: -3)
+		tabGradientView.layer.shadowRadius = 4
+		tabGradientView.layer.shadowColor = UIColor.black.cgColor
+		tabGradientView.layer.shadowOpacity = 0.2
+		tabBar.clipsToBounds = false
+		tabBar.backgroundImage = UIImage()
+		tabBar.shadowImage = UIImage()
 	}
 
 }
