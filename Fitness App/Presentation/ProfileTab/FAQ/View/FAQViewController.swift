6a76993f6835c61ae33680421bd406fc2576f898
Sergey Kletsov <sergey.kletsov@outlook.com>
2019-02-25 13:18:54 +0200

    add faq text loading from server

--- Fitness App/Presentation/ProfileTab/FAQ/View/FAQViewController.swift
+++ Fitness App/Presentation/ProfileTab/FAQ/View/FAQViewController.swift
@@ -11,12 +11,34 @@ import UIKit
 class FAQViewController: UIViewController {
 
 	@IBOutlet private weak var textView: UITextView!
+	private var faqApi: FitnessApi.FAQ!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
 		textView.textContainerInset = .init(top: 20, left: 20, bottom: 20, right: 20)
 		textView.isScrollEnabled = false
+		textView.text = ""
+		
+		faqApi = .init()
+		faqApi.getFaqHtmlString(onComplete: {
+			
+		}, onSuccess: { [weak self] faqHtmlString in
+			self?.updateTextView(with: faqHtmlString)
+		}) { errorText in
+			print(errorText)
+		}
     }
+	
+	private func updateTextView(with newString: String) {
+		DispatchQueue.main.async {
+			let attrStr = try! NSAttributedString(
+				data: (newString.data(using: String.Encoding.unicode, allowLossyConversion: true)!),
+				options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html],
+				documentAttributes: nil)
+			
+			self.textView.attributedText = attrStr
+		}
+	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
